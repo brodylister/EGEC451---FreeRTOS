4c9c9f9ae945ecbe241316a09876458108c30c0d
brodylister <157638806+brodylister@users.noreply.github.com>
2026-02-11 15:46:20 -0800

    question 2
    trying to have two tasks, one that yields to the second.

--- basic_tasks/basic_tasks.ino
+++ basic_tasks/basic_tasks.ino
@@ -15,20 +15,39 @@ struct s_led {
   TaskHandle_t taskh;  // Task handle
 };
 
-static s_led leds[3] = {
-  { LED1, 0, 500, 0 },
-  { LED2, 0, 200, 0 },
-  { LED3, 0, 750, 0 }
+static s_led leds[2] = {
+  { LED1, 0, 20, 0 },
+  { LED2, 0, 100, 0 },
 };
 
-static void led_task_func(void *argp) {
+static void led_task_func_red(void *argp) {
+  s_led *ledp = (s_led *)argp;
+  unsigned stack_hwm = 0, temp;
+
+  delay (1000);
+  
+  for (;;) {
+    digitalWrite(ledp->gpio, ledp->state = 1);      // on at start of task
+    temp = uxTaskGetStackHighWaterMark(nullptr);
+    if (!stack_hwm || temp < stack_hwm) {
+      stack_hwm = temp;
+      printf("Task for gpio %d has stack hwm %u\n",
+             ledp->gpio, stack_hwm);
+    }
+    delay(ledp->napms);
+    digitalWrite(ledp->gpio, ledp->state = 0);      // off after napms
+    taskYIELD();                                    // then yield
+  }
+}
+
+static void led_task_func_green(void *argp) {
   s_led *ledp = (s_led *)argp;
   unsigned stack_hwm = 0, temp;
 
   delay(1000);
 
   for (;;) {
-    digitalWrite(ledp->gpio, ledp->state ^= 1);
+    digitalWrite(ledp->gpio, ledp->state ^= 1);     // forever toggles after napms
     temp = uxTaskGetStackHighWaterMark(nullptr);
     if (!stack_hwm || temp < stack_hwm) {
       stack_hwm = temp;
@@ -50,19 +69,31 @@ void setup() {
          app_cpu > 0 ? "Dual" : "Single");
 
   printf("LEDs on gpios: ");
-  for (auto &led : leds) {
-    pinMode(led.gpio, OUTPUT);
-    digitalWrite(led.gpio, LOW);
-    xTaskCreatePinnedToCore(
-      led_task_func,
-      "led_task",
-      2048,
-      &led,
-      1,
-      &led.taskh,
-      app_cpu);
-    printf("%d ", led.gpio);
-  }
+
+  pinMode(leds[1].gpio, OUTPUT);
+  digitalWrite(leds[1].gpio, LOW);
+  xTaskCreatePinnedToCore(            // red task
+    led_task_func_red,
+    "led_task",
+    2048,
+    &leds[1],
+    1,
+    &leds[1].taskh,
+    app_cpu);
+  printf("%d ", leds[1].gpio);
+  
+  pinMode(leds[2].gpio, OUTPUT);
+  digitalWrite(leds[2].gpio, LOW);
+  xTaskCreatePinnedToCore(            // green task
+    led_task_func_green,
+    "led_task",
+    2048,
+    &leds[2],
+    1,
+    &leds[2].taskh,
+    app_cpu);
+  printf("%d ", leds[2].gpio);
+
   putchar('\n');
 }
 
