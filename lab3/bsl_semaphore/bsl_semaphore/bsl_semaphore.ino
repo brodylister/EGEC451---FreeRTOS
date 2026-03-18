a8b97badbce16310fad351b29095d61ceca0846d
brodylister <157638806+brodylister@users.noreply.github.com>
2026-03-18 16:47:32 -0700

    beginning of lab3
    halfway through question 4 (bsl_semaphore.ino)

---
+++ lab3/bsl_semaphore/bsl_semaphore/bsl_semaphore.ino
@@ -0,0 +1,115 @@
+// Brody Lister, California State University Fullerton
+// Demonstrate use of a locking semaphore
+
+
+/*
+I plan to have one non-reentrant "thinker" task, which will slowly swap two values back and forth, then go to sleep for a while. A second task, the "interrupter", will be modifying a temporary global variable constantly. The "thinker" will use the global variable to swap its two variables, and it should be visible when the data becomes modified unintentionally.
+
+pseudocode:
+
+global int temp;
+
+thinker()       [ non reentrant ]
+ take semaphore
+ set x to 2, y to 4
+ temp = x,
+ delay (critical, could interrupt)
+ x = y, y = temp
+ print x and y
+ give semaphore
+
+interruptor()
+ infinite loop setting temp to 100, 200, ...
+
+
+Printing x and y will show whether their values are successfully swapping back and forth, or if the corrupted data is being used from the interruptor. Both tasks will have a preprocessor directive controlling use of the semaphore macro. I won't print the temp variable to keep it readable.
+
+Expected output without semaphore:
+x is 2, y is 4
+x is 4, y is 2
+x is 2, y is 100
+...
+
+Expected output with semaphore:
+x is 2, y is 4
+x is 4, y is 2
+x is 2, y is 4
+...
+
+*/
+
+#define USE_SEMAPHORE 0
+
+static SemaphoreHandle_t sem;
+static int app_cpu = 0;
+
+BaseType_t global_variable;
+
+
+
+static void interrupterTask() {
+  BaseType_t x = 0;
+  BaseType_t rc;
+
+  for (;;) {
+
+#if USE_SEMAPHORE
+  rc = xSemaphoreTake(sem, portMAX_DELAY);
+  assert(rc == pdPASS);
+#endif
+
+    x = (x+100)%1000 + 100;     // 100, 200, ... 800, 900, 100, ...
+    global_variable = x;
+    delay(10);
+
+#if USE_SEMAPHORE
+  rc = xSemaphoreGive(sem);
+  assert(rc == pdPASS);
+#endif
+
+  } 
+}
+
+static void thinkerTask() {
+  BaseType_t rc;
+
+  for (;;) {
+
+#if USE_SEMAPHORE
+  rc = xSemaphoreTake(sem, portMAX_DELAY);
+  assert(rc == pdPASS);
+#endif
+
+  int x = 2;
+  int y = 4;
+/*
+WIP
+*/
+
+#if USE_SEMAPHORE
+  rc = xSemaphoreGive(sem);
+  assert(rc == pdPASS);
+#endif
+
+  }
+}
+
+
+static void print(int x, int y) {
+  printf("x is %u, y is %u\n",
+  x,
+  y);
+  /*
+  WIP
+  */
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
